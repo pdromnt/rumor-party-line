3a534f6a0e431885e5c273c394a9840b63aa3f3b
Pedro <pedrocarneiromneto@gmail.com>
2025-11-23 04:13:47 -0300

    Deprecate old client

--- client/src/composables/useGetServer.ts
+++
@@ -1,7 +0,0 @@
-export const useGetServer = (): string => {
-  if (Number(import.meta.env.VITE_STATIC)) {
-    return window.location.origin;
-  } else {
-    return import.meta.env.VITE_SERVER;
-  }
-}
--- client/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import PartyViewer from "@/components/PartyViewer.vue";
-import JoinPartyLine from "@/components/JoinPartyLine.vue";
-</script>
-
-<template>
-  <JoinPartyLine/>
-  <PartyViewer/>
-</template>
